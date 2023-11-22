6a88be7248664193e6e396b4f86e315500fa31b6
RBP98 <47711591+RBP98@users.noreply.github.com>
2023-11-22 10:27:59 -0500

    Initialize repository

---
+++ studentSurveyForm/studentSurveyForm/src/main/java/com/swe645/studentSurveyForm/form/SurveyFormRepository.java
@@ -0,0 +1,7 @@
+package com.swe645.studentSurveyForm.form;
+
+import org.springframework.data.repository.CrudRepository;
+public interface SurveyFormRepository extends CrudRepository<Form, String> {
+
+
+}
