dea084c665ea043b4b09032d3623306d375514ec
RBP98 <47711591+RBP98@users.noreply.github.com>
2023-11-27 01:12:33 -0500

    Change directory structure

---
+++ src/main/java/com/swe645/studentSurveyForm/form/Form.java
@@ -0,0 +1,119 @@
+package com.swe645.studentSurveyForm.form;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+@Entity
+public class Form {
+
+    public Form(String id, String firstName, String lastName, String streetAddress, String city, String state, int zip, String telephone, String email, Date date) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.streetAddress = streetAddress;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.telephone = telephone;
+        this.email = email;
+        this.date = date;
+    }
+
+    @Id
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String streetAddress;
+    private String city;
+    private String state;
+
+    private int zip;
+    private String telephone;
+    private String email;
+    private Date date;
+
+    public Form() {
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
