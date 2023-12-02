25dcf008d0bbcd21e19c11b78be523bb016f13e2
RBP98 <47711591+RBP98@users.noreply.github.com>
2023-12-01 22:13:55 -0500

    Update SurveyFormController.java

--- src/main/java/com/swe645/studentSurveyForm/form/controller/SurveyFormController.java
+++ src/main/java/com/swe645/studentSurveyForm/form/controller/SurveyFormController.java
@@ -12,7 +12,7 @@ public class SurveyFormController {
     @Autowired
     private SurveyFormService surveyFormService;
 
-    @RequestMapping("/student")
+    @RequestMapping("/students")
     public List<Form> getAllStudentForms(){
         return surveyFormService.getAllStudentForms();
     }
